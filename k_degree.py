0468e01b33ed1571193928ceb22599d6207f5f59
sahityacodes <you@example.com>
2021-02-28 23:02:05 +0100

    matplot added

--- k_degree.py
+++ k_degree.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from random import randint
 import numpy as np
 import collections
@@ -42,7 +43,6 @@ def put_in_same_group(sequence):
 
 
 def dynamic_programing_graph_anonymization(k, degreeSequence, initialPosition=None):
-
     numberOfNodes = len(degreeSequence)
     anonymizedDegreeSequences = []
     anonymizationCosts = []
@@ -108,65 +108,110 @@ def c_new(d, k):
     return c_new_cost
 
 
-def greedy_rec_algorithm(array_degrees, k_degree, pos_init, extension):
-    if pos_init + extension >= len(array_degrees) - 1:
-        for i in range(pos_init, len(array_degrees)):
-            array_degrees[i] = array_degrees[pos_init]
+def greedy_rec_algorithm(array_degrees_greedy, k_degree, pos_init, extension):
+    if pos_init + extension >= len(array_degrees_greedy) - 1:
+        for i in range(pos_init, len(array_degrees_greedy)):
+            array_degrees_greedy[i] = array_degrees_greedy[pos_init]
         return array_degrees
 
     else:
-        d1 = array_degrees[pos_init]
-        c_merge_cost = c_merge(array_degrees, d1, pos_init + extension)
+        d1 = array_degrees_greedy[pos_init]
+        c_merge_cost = c_merge(array_degrees_greedy, d1, pos_init + extension)
         c_new_cost = c_new(d, pos_init + extension)
         if c_merge_cost > c_new_cost:
             for i in range(pos_init, pos_init + extension):
                 array_degrees[i] = d1
-            greedy_rec_algorithm(array_degrees, k_degree, pos_init + extension, k_degree)
+            greedy_rec_algorithm(array_degrees_greedy, k_degree, pos_init + extension, k_degree)
         else:
-            greedy_rec_algorithm(array_degrees, k_degree, pos_init, extension + 1)
+            greedy_rec_algorithm(array_degrees_greedy, k_degree, pos_init, extension + 1)
 
 if __name__ == "__main__":
-        k_degree = int(sys.argv[1])
-        file_graph = sys.argv[2]
+        timeArr = []
+        # k_degree = int(sys.argv[1])
+        file_graph = sys.argv[1]
+        perfRatio = {}
+        perfRatioOptimised = {}
         G = nx.Graph()
-        G = nx.read_gml(file_graph)
+        G = nx.read_gml(file_graph)    
         d = [x[1] for x in G.degree()]
+        print(len(d))
         array_index = np.argsort(d)[::-1]
         array_degrees =  np.sort(d)[ ::-1 ]
         degreeSumOriginal = sum(array_degrees)
-        start = time.time()
-        cost, array_degrees_DP = dynamic_programing_graph_anonymization(k_degree, np.sort(d)[ ::-1 ])
-        print('time',time.time() - start)
-        degreeSumDynamic = sum(array_degrees_DP)
-        print(array_degrees_DP, degreeSumDynamic)
-        start = time.time()
-        cost, array_degrees_DPO = dynamic_programing_graph_anonymization_opt(k_degree, np.sort(d)[ ::-1 ])
-        print('time',time.time() - start)
-        degreeSumDynamicOptimised = sum(array_degrees_DPO) 
-        print(array_degrees_DP, degreeSumDynamicOptimised)
-        array_degrees_greedy = array_degrees
-        greedy_rec_algorithm(array_degrees_greedy, k_degree, 0, k_degree)
-        degreeSumGreedy = sum(array_degrees_greedy)
+        print('Original Degrees',array_degrees)
 
-        graph_DP = construct_graph(array_index , array_degrees_DP)
-        if graph_DP is not None:
-                    print("Average Clustering:{}".format(nx.average_clustering(graph_DP)))
-        else:
-                    print("Cant construct a Graph for Dynamic Alg")
+        for k_degree in range (3,20):
+            print('K = ',k_degree)
 
-        graph_DP_optimized = construct_graph(array_index , array_degrees_DPO)
-        if graph_DP_optimized is not None:
-                    print("Average Clustering:{}".format(nx.average_clustering(graph_DP_optimized)))
-        else:
-                    print("Cant construct a Graph for Optimized Dynamic Alg")
-                    
-        graph_greedy = construct_graph(array_index, array_degrees_greedy)
-        if graph_greedy is not None:
-                    print("Average Clustering:{}".format(nx.average_clustering(graph_greedy)))
-        else:
-                    print("Cant construct a Graph for Greedy")
+            start = time.time()
+            cost, array_degrees_DP = dynamic_programing_graph_anonymization(k_degree, np.sort(d)[ ::-1 ])
+            print('time',time.time() - start)
+            timeArr.append(time.time() - start)
+            degreeSumDynamic = sum(array_degrees_DP)
+            print(array_degrees_DP, degreeSumDynamic)
 
-        if degreeSumGreedy != 0 and degreeSumDynamic != 0:
-          print('Performance Ratio of Dynamic Alg R =',str((degreeSumGreedy-degreeSumOriginal)/(degreeSumDynamic-degreeSumOriginal)))
-        if degreeSumDynamicOptimised != 0 and degreeSumGreedy != 0:
-          print('Performance Ratio of Optimized Dynamic Alg R1 =',str((degreeSumGreedy-degreeSumOriginal)/(degreeSumDynamicOptimised-degreeSumOriginal)))
+            start = time.time()
+            cost, array_degrees_DPO = dynamic_programing_graph_anonymization_opt(k_degree, np.sort(d)[ ::-1 ])
+            print('time',time.time() - start)
+            timeArr.append(time.time() - start)
+            degreeSumDynamicOptimised = sum(array_degrees_DPO) 
+            print(array_degrees_DPO, degreeSumDynamicOptimised)
+
+            array_degrees_greedy = array_degrees
+            start = time.time()
+            greedy_rec_algorithm(np.sort(d)[ ::-1 ], k_degree, 0, k_degree)
+            print('time',time.time() - start)
+            timeArr.append(time.time() - start)
+            degreeSumGreedy = sum(array_degrees_greedy)
+            print(array_degrees_greedy, degreeSumGreedy)
+            graph_DP = construct_graph(array_index , array_degrees_DP)
+            if graph_DP is not None:
+                        print("Average Clustering:{}".format(nx.average_clustering(graph_DP)))
+            else:
+                        print("Cant construct a Graph for Dynamic Alg")
+
+            graph_DP_optimized = construct_graph(array_index , array_degrees_DPO)
+            if graph_DP_optimized is not None:
+                        print("Nodes in DPO :{}".format(nx.nodes(graph_DP_optimized)))
+                        print("Edges in DPO :{}".format(nx.edges(graph_DP_optimized)))
+            else:
+                        print("Cant construct a Graph for Optimized Dynamic Alg")
+                        
+            graph_greedy = construct_graph(array_index, array_degrees_greedy)
+            if graph_greedy is not None:
+                        print("Nodes in Greedy :{}".format(nx.nodes(graph_greedy)))
+                        print("Edges in Greedy :{}".format(nx.edges(graph_greedy)))
+            else:
+                        print("Cant construct a Graph for Greedy")
+
+            if degreeSumGreedy != 0 and degreeSumDynamic != 0:
+                try:
+                    perfRatio[k_degree] = (degreeSumGreedy-degreeSumOriginal)/(degreeSumDynamic-degreeSumOriginal)
+                except:
+                    perfRatio[k_degree] = 0
+            if  degreeSumGreedy != 0 and degreeSumGreedy != 0:
+                try:
+                    perfRatioOptimised[k_degree] = (degreeSumGreedy-degreeSumOriginal)/(degreeSumDynamicOptimised-degreeSumOriginal)
+                except:
+                    perfRatioOptimised[k_degree] = 0
+            
+        print(perfRatio, perfRatioOptimised)
+            
+        fig = plt.figure()
+        ax = fig.add_axes([0,0,1,1])
+        k = perfRatioOptimised.keys()
+        R = perfRatioOptimised.values()
+        X = np.arange(len(perfRatioOptimised.keys()))
+        plt.xlabel("Degree K")
+        plt.ylabel("Performance Ratio")
+        ax.bar(k,R)
+        plt.show()
+ 
+
+        ax.bar(k,R)
+        plt.show()    
+        
+        
+
+
+       
