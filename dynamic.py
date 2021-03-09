fd5037b09403345ae90575ceb52cd707bb4a7653
Sanchayan Bhunia <sanchayan.bhunia@hotmail.com>
2021-03-10 00:19:40 +0100

    Time Complexity Added

--- dynamic.py
+++ dynamic.py
@@ -1,16 +1,54 @@
 def calculateCost(original_list, modified_list):
-    cost = 0
-    for i in range(len(modified_list)):
-        cost += abs(modified_list[i] - original_list[i])
+    cost = 0 
+    for original, modified in zip(original_list, modified_list):
+        cost += abs(modified - original)
     return cost
 
 def putInSameGroup(sequence):
-    new_sequence = [max(sequence)]*len(sequence)
-    cost = calculateCost(sequence, new_sequence)
-    return cost, new_sequence
+    anonymized_sequence = [max(sequence)]*len(sequence)
+    cost = calculateCost(sequence, anonymized_sequence)
+    return cost, anonymized_sequence
 
 
-def memorizedDynamicProgramingGraphAnonymization(k, degree_sequence, optimization = False, cache=dict()):
+# Dynemic programing algorithm with and wothout Optimization
+def dpGraphAnonymization(k, degree_sequence, optimization = False):
+
+    number_of_nodes = len(degree_sequence)
+    cost_vs_degree_sequences = dict()
+    
+    if k > number_of_nodes or k <= 0:
+        raise Exception("Value of K can not be more than the number of Nodes or less than 0")
+
+    elif k == 1:
+        return 0, degree_sequence
+
+    elif k > number_of_nodes / 2:
+        return putInSameGroup(degree_sequence)
+
+    else:
+        cost_vs_degree_sequences.clear()
+        upper_range = number_of_nodes - k+1
+        if optimization == False :
+            lower_range = k
+        
+        else: 
+            lower_range = max(k, (number_of_nodes - 2*k + 1))
+        
+        for chunk in range(lower_range, upper_range):
+            to_anonymize = degree_sequence[0: chunk]
+            in_same_group = degree_sequence[chunk: number_of_nodes]
+            cost_anonymize, sequence_anonymized = dpGraphAnonymization(k, to_anonymize, optimization)
+            cost_same_group, same_group_squence = putInSameGroup(in_same_group)
+            chunk_cost = cost_anonymize + cost_same_group
+            anonymized_chunk = sequence_anonymized + same_group_squence
+            cost_vs_degree_sequences[chunk_cost] = anonymized_chunk
+            #print((chunk, number_of_nodes), anonymized_chunk)
+        
+        return min(cost_vs_degree_sequences.items(), key = lambda x: x[0])
+
+
+# Dynamic programing algorithm with Memorization
+def memorizedDPGraphAnonymization(k, degree_sequence, optimization = False, cache=dict()):
 
     if not cache:
         cache = dict()
@@ -43,7 +81,7 @@ def memorizedDynamicProgramingGraphAnonymization(k, degree_sequence, optimizatio
                 chunk_cost, anonymized_chunk = record_found[0], record_found[1]
 
             else: 
-                cost_anonymize, sequence_anonymized = memorizedDynamicProgramingGraphAnonymization(k, to_anonymize, optimization, cache)
+                cost_anonymize, sequence_anonymized = memorizedDPGraphAnonymization(k, to_anonymize, optimization, cache)
                 cost_same_group, same_group_squence = putInSameGroup(in_same_group)
                 chunk_cost = cost_anonymize + cost_same_group
                 anonymized_chunk = sequence_anonymized + same_group_squence
@@ -52,40 +90,5 @@ def memorizedDynamicProgramingGraphAnonymization(k, degree_sequence, optimizatio
             cost_vs_degree_sequences[chunk_cost] = anonymized_chunk
 
         return min(cost_vs_degree_sequences.items(), key = lambda x: x[0])
+
     
-
-
-def dynamicProgramingGraphAnonymization(k, degree_sequence, optimization = False):
-
-    number_of_nodes = len(degree_sequence)
-    cost_vs_degree_sequences = dict()
-
-    if k > number_of_nodes:
-        raise Exception("Value of K can not be more than number of Nodes")
-    
-    elif k == 1:
-        return 0, degree_sequence
-
-    elif k > number_of_nodes / 2:
-        return putInSameGroup(degree_sequence)
-
-    else:
-        cost_vs_degree_sequences.clear()
-        upper_range = number_of_nodes - k+1
-        if optimization == False :
-            lower_range = k
-        
-        else: 
-            lower_range = max(k, (number_of_nodes - 2*k + 1))
-        
-        for chunk in range(lower_range, upper_range):
-            to_anonymize = degree_sequence[0: chunk]
-            in_same_group = degree_sequence[chunk: number_of_nodes]
-            cost_anonymize, sequence_anonymized = dynamicProgramingGraphAnonymization(k, to_anonymize, optimization)
-            cost_same_group, same_group_squence = putInSameGroup(in_same_group)
-            chunk_cost = cost_anonymize + cost_same_group
-            anonymized_chunk = sequence_anonymized + same_group_squence
-            cost_vs_degree_sequences[chunk_cost] = anonymized_chunk
-            #print((chunk, number_of_nodes), anonymized_chunk)
-        
-        return min(cost_vs_degree_sequences.items(), key = lambda x: x[0])
